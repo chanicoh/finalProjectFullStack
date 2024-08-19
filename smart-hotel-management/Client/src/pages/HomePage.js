055ed08d2cc7c1aff14e83414e3d910f8600d4e6
chanicoh <chanicoh@g.jct.ac.il>
2024-08-19 13:21:51 +0300

    home page

--- smart-hotel-management/Client/src/pages/HomePage.js
+++ smart-hotel-management/Client/src/pages/HomePage.js
@@ -1,10 +1,28 @@
 // src/pages/HomePage.js
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Css/HomePage.css';
+
 
 function HomePage() {
   return (
-    <div>
-      <h1>Welcome to the Home Page!</h1>
+    <div className="home-page">
+      <div className="top-bar">
+        <Link to="/login" className="login-button">Login</Link>
+      </div>
+      <div className="content">
+        <img 
+          src={require('../assets/hotelHOME.png')} 
+          alt="Hotel Home" 
+          className="home-image"
+        />
+        <h1 className="welcome-message">Welcome to Our Hotel</h1>
+        <div className="navigation">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/about" className="nav-link">About Us</Link>
+          <Link to="/contact" className="nav-link">Contact Us</Link>
+        </div>
+      </div>
     </div>
   );
 }
