b6568fadf97300cef1307c8f4858a62467fe63d3
chanicoh <chanicoh@g.jct.ac.il>
2024-08-19 13:51:35 +0300

    push

--- smart-hotel-management/Client/src/pages/HomePage.js
+++ smart-hotel-management/Client/src/pages/HomePage.js
@@ -5,14 +5,6 @@ import '../Css/HomePage.css';
 function HomePage() {
   return (
     <div className="home-page">
-      <div className="top-bar">
-        <div className="navigation">
-          <Link to="/" className="nav-link">Home</Link>
-          <Link to="/about" className="nav-link">About Us</Link>
-          <Link to="/contact" className="nav-link">Contact Us</Link>
-        </div>
-        <Link to="/login" className="login-button">Login</Link>
-      </div>
       <div className="content">
         <div className="overlay">
           <h1 className="welcome-message">Welcome to Our Hotel</h1>
@@ -23,6 +15,15 @@ function HomePage() {
           className="home-image"
         />
       </div>
+      <div className="top-bar">
+        <div className="navigation">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/about" className="nav-link">About Us</Link>
+          <Link to="/contact" className="nav-link">Contact Us</Link>
+        </div>
+        <Link to="/login" className="login-button">Login</Link>
+      </div>
+      
     </div>
   );
 }
