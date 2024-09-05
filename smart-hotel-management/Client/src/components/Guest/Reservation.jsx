8765fe78b9339a3b9eb8ece6b5dab1c702c9dc4e
chanicoh <chanicoh@g.jct.ac.il>
2024-09-05 16:23:41 +0300

    room

--- smart-hotel-management/Client/src/components/Guest/Reservation.jsx
+++ smart-hotel-management/Client/src/components/Guest/Reservation.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation,useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import '../Css/ReservationPage.css';
+import './ReservationPage.css';
 
 function Reservation() {
   const navigate = useNavigate();
