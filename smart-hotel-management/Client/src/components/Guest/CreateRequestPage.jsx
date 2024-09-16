43edb0983dce6040408f653e0de51450657229bf
ChanaButon <chanabuton@gmail.com>
2024-09-16 09:32:29 +0300

    finishh

--- smart-hotel-management/Client/src/components/Guest/CreateRequestPage.jsx
+++ smart-hotel-management/Client/src/components/Guest/CreateRequestPage.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import queryString from 'query-string';
+import '../../Css/createRequsest.css'; // Updated CSS import
 
 const CreateRequestPage = () => {
+  const navigate = useNavigate();
   const { search } = useLocation();
   const location = useLocation();
   const { room_id } = queryString.parse(search); // Get room_id from query string
@@ -42,27 +44,30 @@ const CreateRequestPage = () => {
 
       const response = await axios.post('/api/servicerequests', newRequest);
       alert('Request submitted successfully!');
+
       setRequestType('');
       setDescription('');
+      navigate('/', { state: { user } });
     } catch (error) {
       setError('Failed to submit request.');
       console.error('Error creating request', error);
     }
   };
 
-  if (error) return <p className="error">{error}</p>;
+  if (error) return <p className="create-request-error">{error}</p>;
 
   return (
-    <div className="request-form">
+    <div className="create-request-form">
       <h2>Create a New Service Request</h2>
       <form onSubmit={handleSubmit}>
-        <div className="form-group">
+        <div className="create-request-form-group">
           <label htmlFor="requestType">Request Type</label>
           <select
             id="requestType"
             value={requestType}
             onChange={(e) => setRequestType(e.target.value)}
             required
+            className="create-request-select"
           >
             <option value="">Select Request Type</option>
             <option value="Order breakfast">Order Breakfast</option>
@@ -72,16 +77,17 @@ const CreateRequestPage = () => {
             <option value="Repair broken light">Repair Broken Light</option>
           </select>
         </div>
-        <div className="form-group">
+        <div className="create-request-form-group">
           <label htmlFor="description">Description</label>
           <textarea
             id="description"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
             required
+            className="create-request-textarea"
           />
         </div>
-        <button type="submit" className="submit-button">Submit Request</button>
+        <button type="submit" className="create-request-submit-button">Submit Request</button>
       </form>
     </div>
   );
